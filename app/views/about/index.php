a84a32b08cad79cebe10d501f53e229e5f6835c4
AkasSakti <146935384+AkasSakti@users.noreply.github.com>
2024-02-22 08:51:11 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,21 @@
+<div class="container mt-3">
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container" align="center">
+			<h1 class="display-4">About Me</h1>
+			<img src="<?= BASEURL ?>/img/husnulm.jpg" alt="Husnul M" width="200" height="250" class="rounded-circle shadow">
+
+			<p class="lead">
+			</p>
+			<table>
+				<tr>
+					<td>Nama Saya</td><td> : </td>
+					<td>Fakultas Vokasi-UB-DIII-TI</td>
+				</tr>
+				<tr>
+					<td>Alamat</td><td> : </td>
+					<td>Kalisongo, Kec. Dau, Kabupaten Malang</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
